3f84e0796d8e1d1e54be67d83824c268e8a0e43b
kajal4414 <81718060+kajal4414@users.noreply.github.com>
2021-07-16 15:41:02 +0530

    spes: packages: Remove packages at build time
    Change-Id: Ic658d7dfba1c962031fdd8e9428095018e7fe6ec
    Signed-off-by: kajal4414 <81718060+kajal4414@users.noreply.github.com>

---
+++ remove_packages/Android.mk
@@ -0,0 +1,27 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+	Camera2 \
+	DeskClock \
+	Eleven \
+	EmergencyInfo \
+	FMRadio \
+	Gallery2 \
+	HTMLViewer \
+	Jelly \
+	Matlog \
+	MusicFX \
+	Nfc \
+	Recorder \
+	SetupWizard \
+	Stk \
+	Tag \
+	Updater
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
