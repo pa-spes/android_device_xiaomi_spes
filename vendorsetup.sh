c25e582e0be1cf13071489b22d557e6b3a626c4c
muralivijay <muralidharan9845@gmail.com>
2024-04-08 12:05:33 +0530

    spes: Fix MiuiCamera not showing on app drawer.
    * Manually clone MiuiCamera apk for fix it.
    
    This reverts commit 22ca7cacc515e2b8404f8320b5455e3143e813cb.
    
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,3 +11,9 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
 # Remove hardware/lineage/compat to avoid conflicts
 rm -rf hardware/lineage/compat/Android.bp
+
+# Remove Miuicamera vendorsetup.sh & Clone Miuicamera.apk manully
+cd vendor/xiaomi/camera/ 
+rm -rf vendorsetup.sh && cd proprietary/system/priv-app/MiuiCamera
+megadl 'https://mega.nz/file/tpsDlTZK#MHn_v46YNN4qZCgX3XGmWY26EIea0NY3yBCTbdebdm0'
+cd ../../../../../../../
