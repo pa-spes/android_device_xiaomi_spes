8b8b577b05cc56de5d48d43b0624757c7d794d51
muralivijay <muralidharan9845@gmail.com>
2024-04-08 12:24:25 +0530

    spes: Remove Old MiuiCamera.apk before clone from megadl
    ERROR: Download failed for 'https://mega.nz/file/tpsDlTZK#MHn_v46YNN4qZCgX3XGmWY26EIea0NY3yBCTbdebdm0': Local file already exists: ./MiuiCamera.apk
    
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,7 +13,7 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 rm -rf hardware/lineage/compat/Android.bp
 
 # Remove Miuicamera vendorsetup.sh & Clone Miuicamera.apk manully
-cd vendor/xiaomi/camera/ 
+cd vendor/xiaomi/camera/
 rm -rf vendorsetup.sh && cd proprietary/system/priv-app/MiuiCamera
-megadl 'https://mega.nz/file/tpsDlTZK#MHn_v46YNN4qZCgX3XGmWY26EIea0NY3yBCTbdebdm0'
+rm -rf MiuiCamera.apk && megadl 'https://mega.nz/file/tpsDlTZK#MHn_v46YNN4qZCgX3XGmWY26EIea0NY3yBCTbdebdm0'
 cd ../../../../../../../
