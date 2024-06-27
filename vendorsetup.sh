42268909e0a215ea5728b1122d0762e15c44c547
kajal4414 <81718060+kajal4414@users.noreply.github.com>
2024-06-27 16:25:52 +0530

    spes: vendorsetup: Track miuicamera
    Signed-off-by: kajal4414 <81718060+kajal4414@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,3 +3,4 @@ rm -rf hardware/google/pixel/kernel_headers
 
 git clone -b lineage-20 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
 git clone -b lineage-20 https://github.com/kajal4414/android_hardware_lineage_compat.git hardware/lineage/compat
+git clone -b uvite-sm6225 --depth=1 https://gitlab.com/ThankYouMario/proprietary_vendor_xiaomi_camera.git vendor/xiaomi/camera
