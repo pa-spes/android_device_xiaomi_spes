e22d608ebafe59ec7e9e487bee5e77587754eeb5
kajal4414 <81718060+kajal4414@users.noreply.github.com>
2024-06-28 20:52:56 +0530

    spes: Remove some kernel configs * These configs not defined in source. * Change TARGET_KERNEL_SOURCE directory. * TARGET_KERNEL_CLANG_COMPILE defaults to true.
    Change-Id: If063e55a2b3eb5d449a4b0f52ec84f8d96ebc3bc
    Signed-off-by: kajal4414 <81718060+kajal4414@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,7 @@
 rm -rf hardware/lineage/compat
 rm -rf hardware/google/pixel/kernel_headers
 
+git clone -b main --depth=1 https://github.com/muralivijay/kernel_xiaomi_sm6225.git kernel/xiaomi/spes
 git clone -b lineage-20 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
 git clone -b lineage-20 https://github.com/kajal4414/android_hardware_lineage_compat.git hardware/lineage/compat
 git clone -b uvite-sm6225 --depth=1 https://gitlab.com/ThankYouMario/proprietary_vendor_xiaomi_camera.git vendor/xiaomi/camera
