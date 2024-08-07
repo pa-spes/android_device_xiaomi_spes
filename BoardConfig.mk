fc695ce9b759b1dacadf32b6e42bff1c8dd57ba8
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-08-07 13:23:33 +0530

    spes: Drop QTI perfd stack
    Change-Id: Ie257a03e66ba86c063f6528329fd7b31c2c75e6f
    Signed-off-by: Kajal4414 <81718060+Kajal4414@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -180,7 +180,6 @@ BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := bengal
 
 # Power
-TARGET_USES_INTERACTION_BOOST := true
 TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 
 # Properties
--- device.mk
+++ device.mk
@@ -368,9 +368,12 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power-service.xiaomi-libperfmgr \
     android.hardware.power@1.3.vendor \
-    android.hardware.power-service-qti \
-    android.hardware.power-V1-ndk_platform.vendor
+    libqti-perfd-client
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Public libraries
 PRODUCT_COPY_FILES += \
@@ -378,10 +381,6 @@ PRODUCT_COPY_FILES += \
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# Perf
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.2.vendor
 
 # QMI
 PRODUCT_PACKAGES += \
