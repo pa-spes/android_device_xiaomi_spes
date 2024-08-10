03f76b62a13904f3509a3ca05f0f4a4c1328e913
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-08-10 17:28:52 +0530

    spes: ArrowOS bringup
    Change-Id: I86fa458a3bf9db88f7b8133600d6fe12ea1bfc2b
    Signed-off-by: Kajal4414 <81718060+Kajal4414@users.noreply.github.com>

--- arrow_spes.mk
+++ arrow_spes.mk
@@ -8,14 +8,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ArrowOS stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Inherit from spes device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
 # Device identifier
-PRODUCT_NAME := lineage_spes
+PRODUCT_NAME := arrow_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
